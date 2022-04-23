134bd6955d758197933e321674aa83c1940a77c2
Vsirotkin <vsirotkin15@gmail.com>
2022-04-23 16:28:14 +0300

    created model user, setup settings, migration

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# own custom field
+AUTH_USER_MODEL = 'accounts.CustomUser'
