e989bb5669c4ba0b020a4149bb06846b214eea08
Vsirotkin <vsirotkin15@gmail.com>
2022-04-23 13:40:34 +0300

    Pipfile, pipfile.lock to docker

--- README.md
+++
@@ -1,5 +0,0 @@
-# building online_book store:
-for it's needed:
-1. Django
-2. Docker
-3. Postgres
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xx&x@ym2r6^fr-dcdwhi$0=5npa#z6_1xbnae_8z90gvl2)+_z'
+SECRET_KEY = 'django-insecure-!z1_3)$$qg%!!^^%u#(4o@r&og1(*#q&2nr9(u^y()3y(3%cu%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
