472f98210fca40207a193844e6a1eb66d7cd748c
Vsirotkin <vsirotkin15@gmail.com>
2022-04-23 18:03:41 +0300

    app accounts
    forms for user, add, change. added user form in admin

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# own custom field
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# own custom field
-AUTH_USER_MODEL = 'accounts.CustomUser'
+
