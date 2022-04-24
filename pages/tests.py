0d624b54419d577df2925fa54e650df803370e0f
Vsirotkin <vsirotkin15@gmail.com>
2022-04-24 12:04:58 +0300

    created tests in tests for existing

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class HomepageTestCase(SimpleTestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+    
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(200, response.status_code)
