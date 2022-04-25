f432a9c4845996e311c26e35423f0d2cdee278c5
Vsirotkin <vsirotkin15@gmail.com>
2022-04-25 15:04:08 +0300

    added url to root url for auth

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('pages.urls')), # main landing page
     path('admin/', admin.site.urls),
+    path('accounts/', include(django.contrib.auth.urls)), #login, logout
+    path('', include('pages.urls')), # main landing page
+    
 ]
